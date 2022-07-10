20f75c0db3269e3f06efa6b4a887cee3e7948213
War Pigeon <54217755+minus1over12@users.noreply.github.com>
2022-07-10 17:07:50 -0500

    Try fixing alt controls again

--- src/main/java/io/github/minus1over12/terraria_activity_tracker/GlobalKeyListener.java
+++ src/main/java/io/github/minus1over12/terraria_activity_tracker/GlobalKeyListener.java
@@ -39,97 +39,96 @@ public class GlobalKeyListener implements NativeKeyListener {
             for (Activity activity : activities)
                 activity.reset();
 
-        if (e.getKeyLocation() != NativeKeyEvent.KEY_LOCATION_NUMPAD) {
 
-            if (!altControls) {
+        if (!altControls) {
 
-                // Figure out toggling the activities. Hardcoded for 25.
-                if (!control && !shift) {
-                    switch (e.getKeyCode()) {
-                        case NativeKeyEvent.VC_F2 -> activities[0].toggle();
-                        case NativeKeyEvent.VC_F3 -> activities[1].toggle();
-                        case NativeKeyEvent.VC_F4 -> activities[2].toggle();
-                        case NativeKeyEvent.VC_F5 -> activities[3].toggle();
-                        case NativeKeyEvent.VC_F6 -> activities[4].toggle();
-                        case NativeKeyEvent.VC_INSERT -> activities[5].toggle();
-                        case NativeKeyEvent.VC_DELETE -> activities[6].toggle();
-                        case NativeKeyEvent.VC_HOME -> activities[7].toggle();
-                        case NativeKeyEvent.VC_END -> activities[8].toggle();
-                        case NativeKeyEvent.VC_PAGE_UP -> activities[9].toggle();
-                        case NativeKeyEvent.VC_PAGE_DOWN -> activities[10].toggle();
-                    }
-                } else if (!control && shift) {
-                    switch (e.getKeyCode()) {
-                        case NativeKeyEvent.VC_F2 -> activities[11].toggle();
-                        case NativeKeyEvent.VC_F3 -> activities[12].toggle();
-                        case NativeKeyEvent.VC_F4 -> activities[13].toggle();
-                        case NativeKeyEvent.VC_F5 -> activities[14].toggle();
-                        case NativeKeyEvent.VC_F6 -> activities[15].toggle();
-                        case NativeKeyEvent.VC_INSERT -> activities[16].toggle();
-                        case NativeKeyEvent.VC_DELETE -> activities[17].toggle();
-                        case NativeKeyEvent.VC_HOME -> activities[18].toggle();
-                        case NativeKeyEvent.VC_END -> activities[19].toggle();
-                        case NativeKeyEvent.VC_PAGE_UP -> activities[20].toggle();
-                        case NativeKeyEvent.VC_PAGE_DOWN -> activities[21].toggle();
-                    }
-                } else if (control && shift) {
-                    switch (e.getKeyCode()) {
-                        case NativeKeyEvent.VC_F2 -> activities[22].toggle();
-                        case NativeKeyEvent.VC_F3 -> activities[23].toggle();
-                        case NativeKeyEvent.VC_F4 -> activities[24].toggle();
-                    }
+            // Figure out toggling the activities. Hardcoded for 25.
+            if (!control && !shift) {
+                switch (e.getKeyCode()) {
+                    case NativeKeyEvent.VC_F2 -> activities[0].toggle();
+                    case NativeKeyEvent.VC_F3 -> activities[1].toggle();
+                    case NativeKeyEvent.VC_F4 -> activities[2].toggle();
+                    case NativeKeyEvent.VC_F5 -> activities[3].toggle();
+                    case NativeKeyEvent.VC_F6 -> activities[4].toggle();
+                    case NativeKeyEvent.VC_INSERT -> activities[5].toggle();
+                    case NativeKeyEvent.VC_DELETE -> activities[6].toggle();
+                    case NativeKeyEvent.VC_HOME -> activities[7].toggle();
+                    case NativeKeyEvent.VC_END -> activities[8].toggle();
+                    case NativeKeyEvent.VC_PAGE_UP -> activities[9].toggle();
+                    case NativeKeyEvent.VC_PAGE_DOWN -> activities[10].toggle();
                 }
-            } else { // Alternate control scheme that consumes the key events, so it does not trigger things in Terraria.
-                if (!shift) {
-                    switch (e.getKeyCode()) {
-                        case NativeKeyEvent.VC_F1 -> activities[0].toggle();
-                        case NativeKeyEvent.VC_F2 -> activities[1].toggle();
-                        case NativeKeyEvent.VC_F3 -> activities[2].toggle();
-                        case NativeKeyEvent.VC_F4 -> activities[3].toggle();
-                        case NativeKeyEvent.VC_F5 -> activities[4].toggle();
-                        case NativeKeyEvent.VC_F6 -> activities[5].toggle();
-                        case NativeKeyEvent.VC_F7 -> {
-                            activities[6].toggle();
-                            consume(e);
-                        }
-                        case NativeKeyEvent.VC_F8 -> {
-                            activities[7].toggle();
-                            consume(e);
-                        }
-                        case NativeKeyEvent.VC_F9 -> activities[8].toggle();
-                        case NativeKeyEvent.VC_F10 -> {
-                            activities[9].toggle();
-                            consume(e);
-                        }
-                        case NativeKeyEvent.VC_F11 -> {
-                            activities[10].toggle();
-                            consume(e);
-                        }
-                        case NativeKeyEvent.VC_INSERT -> activities[11].toggle();
-                        case NativeKeyEvent.VC_DELETE -> activities[12].toggle();
-                        case NativeKeyEvent.VC_HOME -> activities[13].toggle();
-                        case NativeKeyEvent.VC_END -> activities[14].toggle();
-                        case NativeKeyEvent.VC_PAGE_UP -> activities[15].toggle();
-                        case NativeKeyEvent.VC_PAGE_DOWN -> activities[16].toggle();
-                        case NativeKeyEvent.VC_SCROLL_LOCK -> activities[17].toggle();
+            } else if (!control && shift) {
+                switch (e.getKeyCode()) {
+                    case NativeKeyEvent.VC_F2 -> activities[11].toggle();
+                    case NativeKeyEvent.VC_F3 -> activities[12].toggle();
+                    case NativeKeyEvent.VC_F4 -> activities[13].toggle();
+                    case NativeKeyEvent.VC_F5 -> activities[14].toggle();
+                    case NativeKeyEvent.VC_F6 -> activities[15].toggle();
+                    case NativeKeyEvent.VC_INSERT -> activities[16].toggle();
+                    case NativeKeyEvent.VC_DELETE -> activities[17].toggle();
+                    case NativeKeyEvent.VC_HOME -> activities[18].toggle();
+                    case NativeKeyEvent.VC_END -> activities[19].toggle();
+                    case NativeKeyEvent.VC_PAGE_UP -> activities[20].toggle();
+                    case NativeKeyEvent.VC_PAGE_DOWN -> activities[21].toggle();
+                }
+            } else if (control && shift) {
+                switch (e.getKeyCode()) {
+                    case NativeKeyEvent.VC_F2 -> activities[22].toggle();
+                    case NativeKeyEvent.VC_F3 -> activities[23].toggle();
+                    case NativeKeyEvent.VC_F4 -> activities[24].toggle();
+                }
+            }
+        } else { // Alternate control scheme that consumes the key events, so it does not trigger things in Terraria.
+            if (!shift) {
+                switch (e.getKeyCode()) {
+                    case NativeKeyEvent.VC_F1 -> activities[0].toggle();
+                    case NativeKeyEvent.VC_F2 -> activities[1].toggle();
+                    case NativeKeyEvent.VC_F3 -> activities[2].toggle();
+                    case NativeKeyEvent.VC_F4 -> activities[3].toggle();
+                    case NativeKeyEvent.VC_F5 -> activities[4].toggle();
+                    case NativeKeyEvent.VC_F6 -> activities[5].toggle();
+                    case NativeKeyEvent.VC_F7 -> {
+                        activities[6].toggle();
+                        consume(e);
                     }
-                } else {
-                    switch (e.getKeyCode()) {
-                        case NativeKeyEvent.VC_F1 -> activities[18].toggle();
-                        case NativeKeyEvent.VC_F2 -> activities[19].toggle();
-                        case NativeKeyEvent.VC_F3 -> activities[20].toggle();
-                        case NativeKeyEvent.VC_F4 -> activities[21].toggle();
-                        case NativeKeyEvent.VC_F5 -> activities[22].toggle();
-                        case NativeKeyEvent.VC_F6 -> activities[23].toggle();
-                        case NativeKeyEvent.VC_F7 -> {
-                            activities[24].toggle();
-                            consume(e);
-                        }
+                    case NativeKeyEvent.VC_F8 -> {
+                        activities[7].toggle();
+                        consume(e);
+                    }
+                    case NativeKeyEvent.VC_F9 -> activities[8].toggle();
+                    case NativeKeyEvent.VC_F10 -> {
+                        activities[9].toggle();
+                        consume(e);
+                    }
+                    case NativeKeyEvent.VC_F11 -> {
+                        activities[10].toggle();
+                        consume(e);
+                    }
+                    case NativeKeyEvent.VC_INSERT -> activities[11].toggle();
+                    case NativeKeyEvent.VC_DELETE -> activities[12].toggle();
+                    case NativeKeyEvent.VC_HOME -> activities[13].toggle();
+                    case NativeKeyEvent.VC_END -> activities[14].toggle();
+                    case NativeKeyEvent.VC_PAGE_UP -> activities[15].toggle();
+                    case NativeKeyEvent.VC_PAGE_DOWN -> activities[16].toggle();
+                    case NativeKeyEvent.VC_SCROLL_LOCK -> activities[17].toggle();
+                }
+            } else {
+                switch (e.getKeyCode()) {
+                    case NativeKeyEvent.VC_F1 -> activities[18].toggle();
+                    case NativeKeyEvent.VC_F2 -> activities[19].toggle();
+                    case NativeKeyEvent.VC_F3 -> activities[20].toggle();
+                    case NativeKeyEvent.VC_F4 -> activities[21].toggle();
+                    case NativeKeyEvent.VC_F5 -> activities[22].toggle();
+                    case NativeKeyEvent.VC_F6 -> activities[23].toggle();
+                    case NativeKeyEvent.VC_F7 -> {
+                        activities[24].toggle();
+                        consume(e);
                     }
                 }
             }
         }
     }
+
 
     public void nativeKeyReleased(NativeKeyEvent e) {
         switch (e.getKeyCode()) {
@@ -138,9 +137,16 @@ public class GlobalKeyListener implements NativeKeyListener {
             case NativeKeyEvent.VC_F2 -> f2 = false;
             case NativeKeyEvent.VC_F3 -> f3 = false;
         }
+        if (altControls) {
+            switch (e.getKeyCode()) {
+                case NativeKeyEvent.VC_F7, NativeKeyEvent.VC_F8, NativeKeyEvent.VC_F10, NativeKeyEvent.VC_F11 ->
+                        consume(e);
+            }
+        }
     }
 
     //Code from JNativeHook docs
+
     /**
      * Prevents other apps (Terraria) from reading an input event
      */
